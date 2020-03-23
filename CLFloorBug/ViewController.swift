b71726ae8966fd1d7bc2e93f55330c33bf4cb2ce
Robert Ryan <robert.ryan@mindspring.com>
2020-03-23 13:46:00 -0700

    Initial Commit

---
+++ CLFloorBug/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CLFloorBug
+//
+//  Created by Robert Ryan on 3/23/20.
+//  Copyright © 2020 Robert Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
